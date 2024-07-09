e28fd62ba5c77a2860438d7abec579c058c7969e
Eduardo Soares <edurbs@gmail.com>
2024-07-08 22:18:49 -0300

    Initial commit

---
+++ src/main/java/com/company/litera/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.litera.view.user;
+
+import com.company.litera.entity.User;
+import com.company.litera.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
